0e585e23a12b12c6fc8a4685f513a82a26eb5e6a
kuifye <49089769+kuifye@users.noreply.github.com>
2023-06-10 21:57:52 +0800

    2023年6月10日
    更新

--- rime.lua
+++ rime.lua
@@ -1,6 +1,5 @@
 time_translator = require("time")
 select_character_processor = require("select_character")
-auto_select_phrase_processor = require("auto_select_phrase")
 easy_cmd_processor = require("easy_cmd")
 rmdict= require("remove_dict")()
 rm_word_processor=rmdict.processor
